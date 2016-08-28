66d96b846a4d0905d288abe4ccb7a99ae13794b7
thuannp95 <nguyenphuongthuan95@gmail.com>
2016-08-28 19:19:58 +0700

    Update StudentDataAcess.java

--- Bai1/StudentDataAcess.java
+++ Bai1/StudentDataAcess.java
@@ -1,4 +1,4 @@
-package JavaCore.file.io.handle.Bai1;
+package JavaCore.file.io.handle.npt.Bai1;
 
 
 import java.io.*;
@@ -8,57 +8,35 @@ import java.util.List;
  * Created by PhuongThuan on 8/23/2016.
  */
 public class StudentDataAcess {
-    public static void writeToBinaryFile(List<Student> listStudent) {
-        File file = new File("Student.dat");
-        if (file.exists()) {
-            System.out.println("File đã tồn tại LMAO :))");
-            System.exit(0);
-        } else {
-            ObjectOutputStream oos = null;
-            try {
-                oos = new ObjectOutputStream(new FileOutputStream(file));
-                oos.writeObject(listStudent);
-                oos.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+    public static void writeToBinaryFile(List<Student> listStudent) throws IOException {
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("student.dat", false));
+        oos.writeObject(listStudent);
+        oos.close();
     }
 
-    public static List<Student> readFromBinaryFile(String path) throws IOException {
-        File file = new File("Student.dat");
-        if (file.exists()) {
-            System.out.println("File đã tồn tại LMAO :))");
-            System.exit(0);
-        } else {
-            FileInputStream inputStream = new FileInputStream(path);
-            StringBuilder sb = new StringBuilder();
-
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                sb.append(line);
-            }
-            System.out.println("Data from file:\n" + sb);
+    public static List<Student> readFromBinaryFile(String path) throws IOException, ClassNotFoundException {
+        ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(new FileInputStream(path)));
+        List<Student> list = (List<Student>) ois.readObject();
+        for (Student st1 : list) {
+            System.out.println(st1.infoStudent());
         }
+        ois.close();
         return null;
     }
 
     public static void writeToTextFile(List<Student> listStudent) throws IOException {
-        File file = new File("Student.txt");
-        if (file.exists()) {
-            System.out.println("File đã tồn tại LMAO :))");
-            System.exit(0);
-        }
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
-        for (Student student : listStudent) {
-            bufferedWriter.write(student.toString());
-            bufferedWriter.write("\n");
-        }
-        bufferedWriter.close();
+        ObjectOutputStream oos2 = new ObjectOutputStream(new FileOutputStream("studenttext.txt", false));
+        oos2.writeObject(listStudent);
+        oos2.close();
     }
 
-    public static List<Student> readFromTextFile(String path) {
+    public static List<Student> readFromTextFile(String path) throws IOException, ClassNotFoundException {
+        ObjectInputStream ois1 = new ObjectInputStream(new BufferedInputStream(new FileInputStream("studenttext.txt")));
+        List<Student> list1 = (List<Student>) ois1.readObject();
+        for (Student st2 : list1) {
+            System.out.println(st2.infoStudent());
+        }
+        ois1.close();
         return null;
     }
 }
